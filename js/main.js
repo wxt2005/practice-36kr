b13536872cbddafd6054ea9531a99612fc061072
Botao <wxt2005@gmail.com>
2014-09-23 21:45:10 +0800

    add side bar tabs click event

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@
 
 $(function() {
     var $window = $(window);
-    
+    var $sideBarTab = $('.sidebar-tab .tabs');
+    var $sideBarTabContents = $('.sidebar-tab .tabs-content');
+    var i = 0, l = 0;
+
     /**
      * 页面滚动事件
      */
@@ -18,5 +21,26 @@ $(function() {
         } else {
             $('header').removeClass('scrolled');
         }
+    });
+
+    /**
+     * 侧边栏标签切换事件
+     */
+    $sideBarTab.click(function(ev) {
+        var index = -1;
+        var $target = $(ev.target);
+        $(this).find('li').removeClass('selected');
+        if ($target.is('a')) {
+            index = $target.parent().index();
+            $target.parent().addClass('selected');
+        } else {
+            index = $target.index();
+            $target.addClass('selected');
+        }
+        for (i = 0, l = $sideBarTabContents.length; i < l; i++) {
+            $sideBarTabContents[i].style.display = "none";
+        }
+        $sideBarTabContents[index].style.display = "block";
+        return false;
     });
 });
